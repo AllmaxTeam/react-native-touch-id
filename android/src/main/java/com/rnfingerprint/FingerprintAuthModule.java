71b36c756a66f113d550fe0cfb4c73e0e1adae29
Dmitriy Shishkin <dshishkin@allmax.team>
2018-04-12 12:14:04 +0700

    getReactApplicationContext

--- android/src/main/java/com/rnfingerprint/FingerprintAuthModule.java
+++ android/src/main/java/com/rnfingerprint/FingerprintAuthModule.java
@@ -52,9 +52,9 @@ public class FingerprintAuthModule extends ReactContextBaseJavaModule {
   @ReactMethod
   public void isSupported(Callback reactErrorCallback, Callback reactSuccessCallback) {
     keyguardManager =
-            (KeyguardManager) getCurrentActivity().getSystemService(Context.KEYGUARD_SERVICE);
+            (KeyguardManager) getReactApplicationContext().getSystemService(Context.KEYGUARD_SERVICE);
     fingerprintManager =
-            (FingerprintManager) getCurrentActivity().getSystemService(Context.FINGERPRINT_SERVICE);
+            (FingerprintManager) getReactApplicationContext().getSystemService(Context.FINGERPRINT_SERVICE);
     if(!isFingerprintAuthAvailable()) {
       reactErrorCallback.invoke("Not supported.");
     } else {
